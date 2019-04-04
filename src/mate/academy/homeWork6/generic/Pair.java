7bd25b9d8cb0a12c05cf58852a8e62fb5991cfc1
Illia1927 <wides544@gmail.com>
2019-04-04 19:18:46 +0300

    add generic home work

---
+++ src/mate/academy/homeWork6/generic/Pair.java
@@ -0,0 +1,40 @@
+package mate.academy.homeWork6.generic;
+
+import java.util.Objects;
+
+public class Pair<F, S> {
+    private F first;
+
+    private S second;
+
+    private Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    public static Pair of(Object first, Object second){
+        return new Pair(first, second);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pair)) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(getFirst(), pair.getFirst()) &&
+                Objects.equals(getSecond(), pair.getSecond());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirst(), getSecond());
+    }
+}
